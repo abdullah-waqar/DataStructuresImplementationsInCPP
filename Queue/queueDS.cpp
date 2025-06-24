e2aec5d49446c6246b59ab2bfff7a06f632ba1c4
abdullah-waqar <abdullahwaqar803@gmail.com>
2025-06-24 03:09:52 -0700

    Refactor Queue implementation to enhance functionality with complete methods for enqueue, dequeue, isEmpty, isFull, count, and display.

--- Queue/queueDS.cpp
+++ Queue/queueDS.cpp
@@ -1,43 +1,158 @@
 #include <iostream>
 using namespace std;
 
-// Queue -> First in First out "or" last in last out
-
-// insertiong -> from behind rear / tail
-// deletion -> from front  front / head
-
-// rear/tail -> points to the last element that we inserted
-// front/head -> points to the first element that we delete
-
-//? QUEUE OPERATIONS
-   //? ENQUEUE, DEQUEUE, PEEK, ISFULL, ISEMPTY
-
-
-//! Enqueue
-    //! 1. Check if the queue is full
-    //! 2. if Queue is full , then exist
-    //! 3. if not, increment rear pointer by one
-    //! 4. Insert the element where the rear pointer is pointing
-
-int queue[5];
-int rear = -1, front = -1;
-
-void enqueue(int x)
-{
-    if(rear == -1)
-        front++;
-
-    if(rear != 4)
+class Queue{
+    private:
+        int front;
+        int rear;
+        int arr[5];
+    public:
+    Queue()
     {
-        rear++;
-        queue[rear] = x;
+        front = rear = -1;
+        for(int i = 0; i < 5; i++)
+        {
+            arr[i] = 0;    
+        }
     }
-    else {
-        cout << "Queue us full" << endl;
+
+    // isEmpty function
+    bool isEmpty()
+    {
+        if(rear == -1 && front == -1)
+            return true;
+        else
+            return false;
     }
-}
+
+    // isFull Function
+
+    bool isFull()
+    {
+        if(rear == 4)
+            return true;
+        else
+            return false;
+    }
+
+    void enqueue(int val)
+    {
+        if(isFull())
+        {
+            cout << 'Queue us full' << endl;
+            return;
+        }
+        else if(rear == -1 && front == -1)
+            rear = front = 0;
+        else
+            rear++;
+
+        arr[rear] = val;
+    }
+
+    int dequeue()
+    {
+        int x = 0;
+        if(isEmpty())
+        {
+            cout << "Queue is Empty" << endl;
+            return 0;
+        }
+        else if(rear == front)
+        {
+            x = arr[front];
+            arr[front] = 0;
+            front = rear = -1;
+
+        }
+        else {
+            x = arr[front];
+            arr[front] = 0;
+            front++;
+        }
+
+        return x;
+    }
+
+    int count()
+    {
+        return (rear - front) +1 ;
+    }
+
+    void display()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            cout << arr[i] << endl;
+        }
+    }
+
+};
 
 int main()
 {
+    Queue q1;
+    int option , value;
+    do
+    {  
+        cout << "Enter what operation you want to perform? Select option number. Enter 0 to exit" << endl;
+        
+        cout << "1. Enqueue()" << endl;
+
+        cout << "2. Dequeue()" << endl;
+
+        cout << "3. isEmpty()" << endl;
+
+        cout << "4. isFull()" << endl;
+
+        cout << "5. count()" << endl;
+
+        cout << "6. display()" << endl;
+
+        cout << "7. Clear Screen" << endl;
+
+        cin >> option;
+
+        switch (option)
+        {
+        case 0:
+            break;
+        case 1:
+           cout << "Enqueue Operation called - Enter a value: ";
+           cin >> value;
+           q1.enqueue(value);
+            break;
+        case 2:
+            cout << "Dequeue operation called " << q1.dequeue() << endl;
+            break;
+        case 3:
+            if(q1.isEmpty())
+                cout << "Queue is Empty" << endl;
+            else
+                cout << "Queue is not Empty" << endl;
+            break;
+        case 4:
+            if(q1.isFull())
+                cout << "Queue is Full" << endl;
+            else 
+                cout << "Queue is not Full" << endl;
+            break;
+        case 5:
+            cout << "Items in queue are: " << q1.count() << endl;
+            break;
+        case 6:
+            cout << "Display function called, values in queue are " << endl;
+            q1.display();
+            break;
+        case 7:
+            system("cls");
+            break;
+        
+        default:
+            cout << "Enter valid option" << endl;
+            break;
+        }
+        
+    } while (option != 0);
     
 }
