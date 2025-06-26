7a49c223890212f16364f07578f0c131a9e293a5
abdullah-waqar <abdullahwaqar803@gmail.com>
2025-06-26 13:40:13 -0700

    Add factorial and power functions with a main function for testing

---
+++ Recursion/first.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n == 0)
+        return 1;
+    
+    int ans = n * factorial(n-1);
+    return ans;
+
+}
+
+int power(int base, int exp)
+{
+    if(exp == 0)
+        return 1;
+
+    return base * power(base , exp - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    // int ans = factorial(n);
+    // cout << "Answer: " << ans << endl;
+
+    int ans = power(2, 2);
+    cout << "Answer: " << ans << endl;
+
+}
