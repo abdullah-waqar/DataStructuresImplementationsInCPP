a69ebdd95fe16ae5fe91fb56c7abc883c775ebeb
abdullah-waqar <abdullahwaqar803@gmail.com>
2025-07-24 21:57:29 -0700

    Refactor majority element algorithm by removing unused code and comments for clarity

--- Arrays/03_majority_element.cpp
+++ Arrays/03_majority_element.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 #include <algorithm>
-
+#include <unordered_map>
+#include <limits>
 using namespace std;
 
 int main()
 {
-    int arr[] = {3,2,3};
-    sort(arr, arr + sizeof(arr) / sizeof(arr[0]));
+    // int arr[] = {3,2,3};
+    // sort(arr, arr + sizeof(arr) / sizeof(arr[0]));
 
-    int freq = 1;
-    int ans;
-    for(int  i = 1; i < 3; i++)
+    // int freq = 1;
+    // int ans;
+    // for(int  i = 1; i < 3; i++)
+    // {
+    //     if(arr[i] == arr[i-1])
+    //     {
+    //         freq++;
+
+    //     }
+    //     else {
+    //         freq = 1;
+    //         ans = arr[i];
+    //     }
+
+    //     if(freq > 3/2)
+    //     {
+    //         cout << "Majority element is: " << ans << endl;
+    //         break;
+    //     }
+
+    // }
+
+    int arr[] = {4,5,3,4,4,3,4,4,4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int freq = 0, ans = 0;
+    for(int i = 0; i < size; i++)
     {
-        if(arr[i] == arr[i-1])
-        {
-            freq++;
-
-        }
-        else {
-            freq = 1;
+        if(freq == 0)
             ans = arr[i];
-        }
-
-        if(freq > 3/2)
-        {
-            cout << "Majority element is: " << ans << endl;
-            break;
-        }
+        
+       ans == arr[i] ? freq++ : freq--;
 
     }
+
+    cout << "Majority element is: " << ans << endl;
 }
